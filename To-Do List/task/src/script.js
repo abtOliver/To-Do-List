926795c8369f2aad48fa38fdf47332d5365cdc44
Oliver G. Abt <abto@lars-net.de>
2021-12-30 17:32:28 +0100

    Improve Functionality

--- To-Do List/task/src/script.js
+++ To-Do List/task/src/script.js
@@ -1,46 +1,63 @@
-let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-for (const task of taskList) {
-    addTask(task);
+let taskArray = JSON.parse(localStorage.getItem("tasks")) || [];
+for (const task of taskArray) {
+    addTaskToView(task);
 }
 
 document.getElementById("add-task-button")
     .addEventListener("click", function () {
         let inputTask = document.getElementById("input-task");
         if (inputTask.value === "") {
-            console.log("empty input");
+            console.log("Empty input");
             return;
         }
-        console.log("Task: " + inputTask.value);
-        addTask(inputTask.value);
-        saveTask(inputTask.value);
+
+        let task = {text: inputTask.value, done: false};
+        addTaskToView(task);
+        taskArray.push(task);
+        saveTasks(taskArray)
+
         inputTask.value = "";
     });
 
-function addTask(task) {
+function addTaskToView(task) {
+    console.log("Task: " + taskToString(task));
+
+    let taskListElement = document.getElementById("task-list");
+    let taskIndex = taskListElement.childElementCount;
+
     let inputElement = document.createElement("input");
     inputElement.type = "checkbox";
+    inputElement.checked = task.done;
+    inputElement.addEventListener("click", function () {
+        task.done = !task.done;
+        saveTasks(taskArray);
+        console.log("Task: " + taskToString(task))
+    });
 
     let spanElement = document.createElement("span");
     spanElement.className = "task";
-    spanElement.innerHTML = task;
+    spanElement.innerHTML = task.text;
 
     let buttonElement = document.createElement("button");
     buttonElement.className = "delete-btn";
     buttonElement.innerHTML = "del";
+    buttonElement.addEventListener("click", function () {
+        console.log("Remove task: " + taskToString(task));
+        taskListElement.removeChild(listElement);
+        taskArray.splice(taskIndex, 1);
+        saveTasks(taskArray);
+    });
 
     let listElement = document.createElement("li");
     listElement.append(inputElement, spanElement, " ", buttonElement);
 
-    document.getElementById("task-list")
-        .append(listElement);
-
-    buttonElement.addEventListener("click", function () {
-        document.getElementById("task-list")
-            .removeChild(listElement);
-    });
+    taskListElement.append(listElement);
 }
 
-function saveTask(task) {
-    taskList.push(task);
-    localStorage.setItem("tasks", JSON.stringify(taskList))
+function taskToString(task) {
+    return task.text + (task.done ? " (done)" : " (open)");
+}
+
+function saveTasks(taskArray) {
+    localStorage.setItem("tasks", JSON.stringify(taskArray));
 }
